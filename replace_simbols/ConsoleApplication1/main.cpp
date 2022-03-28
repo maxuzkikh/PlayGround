6e2086e4ab51692215b5ac71ed9c125501da1c5c
Max Uzkikh <maxuzkikh@gmail.com>
2022-03-28 16:09:04 +0500

    replace_simbols

---
+++ replace_simbols/ConsoleApplication1/main.cpp
@@ -0,0 +1,26 @@
+﻿// CPP program for the above approach
+#include <cstring>
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Driver Code
+int main()
+{
+	string text = "aabbccabadef";
+	char* char_arr;
+	char_arr = &text[0];
+	int n = 0;
+
+	for (int i = 0; i < text.length(); i++) {
+			if (char_arr[i] == 'a' || char_arr[i] == 'a') {
+				char_arr[i] = 'b';
+				n++;
+			}
+	}
+
+	cout << text << endl; 
+	cout << n << endl;
+	system("pause>0");
+	return 0;
+}
